1e9ad732736a4319a4cfe8f2934590f29c7e6ccf
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-03 18:27:03 +0530

    Time: 41 ms (1.7%), Space: 9 MB (94.67%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]) return s[i];
+        char ans;
+        for(int i=0;i<t.size();i++){
+        if(count(s.begin(), s.end(), t[i]) != count(t.begin(), t.end(), t[i]) )ans=t[i];
         }
-        return t[t.size()-1];
+        return ans;
     }
 };
