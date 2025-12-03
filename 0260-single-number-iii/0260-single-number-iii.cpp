c00f353b258587a3464949f1a34989101d04a14f
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-03 16:37:06 +0530

    Time: 2 ms (33.4%), Space: 13.9 MB (82.63%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& arr) {
+        vector<int>ans;
+        sort(arr.begin(),arr.end());
+
+        if(arr.size()==1) ans.push_back(arr[0]);
+        if(arr[0]!=arr[1]) ans.push_back(arr[0]);
+        if(arr[arr.size()-1]!=arr[arr.size()-2]) ans.push_back(arr[arr.size()-1]);
+        for(int i=1;i<arr.size()-1;i++){
+            if(arr[i]!=arr[i-1] && arr[i]!=arr[i+1]) ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
