eba6850d8707ac481fb28ca1071e5fafd0b1ac91
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-03 16:29:29 +0530

    Time: 9 ms (16.36%), Space: 20.6 MB (83.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
         int ans;
-        for(int i=0;i<arr.size();i++){
-            int sum=0;
-            for(int j=0;j<arr.size();j++){
-                if(arr[i]==arr[j] && i!=j)sum++;
-            }
-            if(sum==0) ans=arr[i];
+        if(arr.size()==1) return arr[0];
+        if(arr[0]!=arr[1]) return arr[0];
+        if(arr[arr.size()-1]!=arr[arr.size()-2]) return arr[arr.size()-1];
+        for(int i=1;i<arr.size()-1;i++){
+            if(arr[i]!=arr[i-1] && arr[i]!=arr[i+1]) ans=arr[i];
         }
+        // int ans;
+        // for(int i=0;i<arr.size();i++){
+        //     int sum=0;
+        //     for(int j=0;j<arr.size();j++){
+        //         if(arr[i]==arr[j] && i!=j)sum++;
+        //     }
+        //     if(sum==0) ans=arr[i];
+        // }
         return ans;
     }
 };
