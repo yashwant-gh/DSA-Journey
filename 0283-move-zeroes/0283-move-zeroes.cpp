3788d3a49169a8826eef69e164cada585453586a
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-04 15:33:12 +0530

    Time: 3 ms (16.25%), Space: 23.9 MB (19.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
         int coun=count(arr.begin(),arr.end(),0);
-       auto it= remove(arr.begin(),arr.end(),0);
-       arr.erase(it,arr.end());
-        for(int i=0;i<coun;i++){
-            arr.push_back(0);
-        }
-
+        auto it= remove(arr.begin(),arr.end(),0);
+        arr.erase(it,arr.end());
+        for(int i=0;i<coun;i++) arr.push_back(0);
     }
 };
