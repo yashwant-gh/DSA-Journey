edde80ccf53c1eb1a36754f67d3e5fa297f08345
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-10 21:23:18 +0530

    Time: 0 ms (100%), Space: 9.1 MB (28.85%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string getHint(string sc, string gs) {
+        int x=0,y=0;
+        unordered_map<char,int>mp;
+        
+        for(int i=0;i<sc.size();i++){
+            if(sc[i]==gs[i]){
+                x++;
+                sc[i]='X';
+                gs[i]='Y';
+            }
+        }
+        for(char c:gs){
+            mp[c]++;
+        }
+        for(int i=0;i<sc.size();i++){
+            if(mp[sc[i]]>0) {
+                y++;
+                mp[sc[i]]--;
+            }
+        }
+        string X= to_string(x);
+        string Y= to_string(y);
+        string ans="";
+        ans+=X;
+        ans+='A';
+        ans+=Y;
+        ans+='B';
+        return ans;
+    }
+};
