e465e179006c602b1e55c116b70b901fe79df0ef
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-24 20:51:39 +0530

    Time: 3 ms (62.26%), Space: 8 MB (19.38%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+vector<int>dp;
+Solution(){
+    dp.resize(31,-1);
+}
+    int fib(int n) {
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=fib(n-1)+fib(n-2);
+    }
+};
