0b679d40052973aeb390bca998de2d68bb4eb805
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-25 14:27:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& arr, int k) {
+        sort(arr.begin(),arr.end());
+        long long sum=0;
+        for(int i=0;i<k;i++){
+            if(arr.back()==0) break;
+            sum+=arr.back()-i;
+            arr.pop_back();
+        }
+        // while(k){
+        //     sum+=arr.back();
+        //     arr.pop_back();
+        //     for(int &x:arr){
+        //         if(x>0) x--;
+        //     }
+        //     k--;
+        // }
+        return sum;
+    }
+};
