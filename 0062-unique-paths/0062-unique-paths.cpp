b40159b6c1bffabd4ab7192dcbdbe1d415d7c1b9
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-25 13:26:53 +0530

    Time: 0 ms (100%), Space: 11.8 MB (27.22%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+vector<vector<int>>dp;
+Solution(){
+    dp.resize(101,vector<int>(101,-1));
+}
+    int solve(int i, int j) {
+        if(i<0 || j<0) return 0;
+        if(i == 0 || j == 0) return dp[i][j]=1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=solve(i-1,j)+solve(i,j-1);
+    }
+
+    int uniquePaths(int i,int j){
+        return solve(i-1,j-1);
+    }
+};
