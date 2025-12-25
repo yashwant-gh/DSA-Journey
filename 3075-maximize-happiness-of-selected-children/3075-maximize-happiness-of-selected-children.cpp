2f2fed6d38dca9fe8540df9f9f033d6e785da840
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-25 14:30:11 +0530

    Time: 163 ms (45.39%), Space: 108 MB (47.16%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -4,7 +4,7 @@ public:
         sort(arr.begin(),arr.end());
         long long sum=0;
         for(int i=0;i<k;i++){
-            if(arr.back()==0) break;
+            if(arr.back()-i<0) break;
             if((arr.back()-i)>=0) sum+=arr.back()-i;
             arr.pop_back();
         }
