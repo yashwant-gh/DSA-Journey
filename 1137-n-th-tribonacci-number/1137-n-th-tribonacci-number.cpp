b6feed3d9978799eacda2ce7bc23307f4840fcd0
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2025-12-25 13:39:04 +0530

    Time: 0 ms (100%), Space: 8 MB (47.9%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+vector<int>dp;
+Solution(){
+    dp.resize(38,-1);
+}
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    }
+};
