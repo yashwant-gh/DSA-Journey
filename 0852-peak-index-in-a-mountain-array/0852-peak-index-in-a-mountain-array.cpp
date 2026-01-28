254d49570836b8ab3661f1a8a0a096cd6c193351
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-01-29 01:13:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int bs(vector<int>& arr){
+    int l=0;
+    int h=arr.size()-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+            return mid;
+        }
+        else if (arr[mid]>arr[mid-1])l=mid+1;
+        else h=mid-1;
+    }
+    return -1;
+}
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int ans=bs(arr);
+        return ans;
+    }
+};
