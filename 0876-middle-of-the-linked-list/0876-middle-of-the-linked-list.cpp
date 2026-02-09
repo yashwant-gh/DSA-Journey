4ded7699013aad35ef2659af335a78b81be07679
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-02-09 19:19:14 +0530

    Time: 0 ms (100%), Space: 10 MB (25.4%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -10,40 +10,16 @@
  */
 class Solution {
 public:
-    class Node{
-        public:
-        int data;
-        Node* next;
-        Node(int value){
-            data=value;
-            next=NULL;
-        }
-    };
-
-    Node* LL(int arr[],int index,int size){
-       if(index==size) return NULL;
-       Node* temp= new Node(arr[index]);
-       temp->next=LL(arr,index+1,size);
-       return temp;
-    }
-
     ListNode* middleNode(ListNode* head) {
-        Node* Head=(Node*)head;
-        Node*tp=Head;
-        int size=0;
-        while(tp){
-            tp=tp->next;
-            size++;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast->next){
+            slow=slow->next;
+            fast=fast->next;
+            if(!fast->next)break;
+            fast=fast->next;
+            if(!fast->next)break;
         }
-        Node*tp2=Head;
-        int i=1;
-        while(tp2){
-            if(i==(size/2)+1){
-                return (ListNode*)tp2;
-            }
-            tp2=tp2->next;
-            i++;
-        }
-        return NULL;
+        return slow;
     }
 };
