007156c9204324b55ec3efa639d897d521318f7a
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-05-21 21:44:38 +0530

    Time: 88 ms (87.73%), Space: 88.5 MB (28.37%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -19,6 +19,7 @@ public:
             mx = root->val;
             ans = 1;
         }
+        else ans = 0;
         ans += good(root->left, mx);
         ans += good(root->right, mx);
         return ans;
