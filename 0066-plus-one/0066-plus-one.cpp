5c3abe567e6dbb5f4476824e6fd4052d79f93efd
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-06-25 00:25:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (26.85%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& arr) {
+        arr[arr.size()-1]++;
         int carry = 0;
         int sum = 0;
         for(int i=arr.size()-1;i>=0;i--){
-            if(i==arr.size()-1)sum = 1 + arr[i];
-            else sum = carry + arr[i];
+            // if(i==arr.size()-1)sum = 1 + arr[i];
+            sum = carry + arr[i];
             if(sum > 9){
                 carry = 1;
                 arr[i] = sum - 10;
