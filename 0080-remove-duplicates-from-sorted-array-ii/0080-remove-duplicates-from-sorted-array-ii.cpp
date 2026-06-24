db92bd502d5118a0cde8c51202fae08cae2b27ca
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-06-24 22:46:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (96.78%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& arr) {
-        unordered_map<int,int>mp;
-        set<int>st;
-        int count=0;
+        int k = 0;
         for(int x:arr){
-            mp[x]++;
-            if(mp[x]>2) mp[x]=2;
-            st.insert(x);
-        }
-        int i=0;
-        for(int x: st){
-            count+=mp[x];
-            while(mp[x]){
-                arr[i]=x;
-                mp[x]--;
-                i++;
+            if(k<2 || x!= arr[k-2]){
+                arr[k++]=x;
             }
         }
-
-        return count;
-
+        return k;
     }
 };
