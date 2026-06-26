39bb779ba339ba9bb99c8e20b4674910d3ba95fa
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-06-26 23:23:47 +0530

    Time: 64 ms (10.25%), Space: 78.4 MB (9.74%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map<int,int>mp;
         for(int x:arr)mp[x]++;
         for(int i=1;i<=arr.size();i++){
-            if(!mp[i]){
-                return i;
-            }
+            if(!mp[i])return i;
         }
         return arr.size()+1;
     }
