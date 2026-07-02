414f75fb79cad91a80ba8481ae0396c6c7118f6b
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-07-02 14:35:25 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t)return false;
+        if(s.size()!=t.size())return false;
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(char c:s)mp1[c]++;
+        for(char c:t)mp2[c]++;
+        for(int i=0;i<s.size();i++){
+            if(mp1[s[i]]!=mp2[s[i]])return false;
+        }
         return true;
     }
 };
