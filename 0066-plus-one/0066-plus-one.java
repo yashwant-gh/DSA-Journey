fe7f82288d1c501bda776b916c89b2f04bb97708
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-07-19 14:33:58 +0530

    Time: 0 ms (100%), Space: 43.4 MB (62.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] plusOne(int[] arr) {
-        int carry = 1, sum =0;
-        ArrayList <Integer> ans = new ArrayList<>();
-        for(int i = arr.length -1; i>=0;i--){
-            sum =carry + arr[i];
-            int digit  = sum%10;
-            ans.add(0,digit);
-            carry = sum>9?1:0;
+        for(int i = arr.length-1;i>=0;i--){
+            if(arr[i]<9){
+                arr[i]++;
+                return arr;
+            }
+            arr[i]=0;
         }
-        if(carry==1)ans.add(0,1);
-        int [] ans1 =new int[ans.size()];
-        for(int i=0;i<ans.size();i++)ans1[i] = ans.get(i);
-        return ans1;
+        arr = new int[arr.length+1];
+        arr[0]=1;
+        return arr;
     }
 }
