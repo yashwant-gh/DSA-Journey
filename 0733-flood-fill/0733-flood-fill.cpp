69ef551a782bcb172b576169c4562bd09a5755b5
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-07-24 14:49:08 +0530

    Time: 0 ms (100%), Space: 18.1 MB (76.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,8 +3,7 @@ public:
     void dfs(int r, int c, vector<vector<int>>&image, int original, int color){
         int n = image.size();
         int m =image[0].size();
-        if(r<0 || r>n-1 ||c<0||c>m-1)return;
-        if(image[r][c]!=original)return;
+        if(r<0 || r>n-1 ||c<0||c>m-1 || image[r][c]!=original)return;
         image[r][c] = color;
         dfs(r-1,c,image,original,color);
         dfs(r+1,c,image,original,color);
