6884163c992a4d1b8c102017e62fbac3123b04e4
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-07-24 14:48:13 +0530

    Time: 0 ms (100%), Space: 18 MB (76.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-
-bool isvalid(vector<vector<int>>& arr,int r, int c,int val,int color, int m, int n){
-    if(r<0 || c<0 || c>=n || r>=m || arr[r][c]==color || arr[r][c]!=val) return false;
-    return true;
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int color) {
-        int m=arr.size();
-        int n=arr[0].size();
-        queue<pair<int,int>>q;
-        int val=arr[sr][sc];
-        arr[sr][sc]=color;
-        q.push({sr,sc});
-        while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            if(isvalid(arr,r-1,c,val,color,m,n)){
-                arr[r-1][c]=color;
-                q.push({r-1,c});
-            }
-            if(isvalid(arr,r+1,c,val,color,m,n)){
-                arr[r+1][c]=color;
-                q.push({r+1,c});
-            }
-            if(isvalid(arr,r,c-1,val,color,m,n)){
-                arr[r][c-1]=color;
-                q.push({r,c-1});
-            }
-            if(isvalid(arr,r,c+1,val,color,m,n)){
-                arr[r][c+1]=color;
-                q.push({r,c+1});
-            }
-        }
-        return arr;
+    void dfs(int r, int c, vector<vector<int>>&image, int original, int color){
+        int n = image.size();
+        int m =image[0].size();
+        if(r<0 || r>n-1 ||c<0||c>m-1)return;
+        if(image[r][c]!=original)return;
+        image[r][c] = color;
+        dfs(r-1,c,image,original,color);
+        dfs(r+1,c,image,original,color);
+        dfs(r,c-1,image,original,color);
+        dfs(r,c+1,image,original,color);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int original  = image[sr][sc];
+        if(original == color)return image;
+        dfs(sr,sc,image,original,color);
+        return image;
     }
 };
